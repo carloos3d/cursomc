26da3808e84e9dfecfcbf25544e52c8fb8e85b2f
carloos3d <tec.carlos.macedo@outlook.com>
2019-08-12 16:58:56 -0300

    testando a primiera classe

--- src/main/java/com/example/demo/resources/CategoriaResources.java
+++ src/main/java/com/example/demo/resources/CategoriaResources.java
@@ -1,8 +1,16 @@
 package com.example.demo.resources;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,10 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResources {
 	@RequestMapping(method = RequestMethod.GET)
 	
-	public String listar()
+	public List<Categoria> listar()
 	{
 		  
-		return "REST está funcionando";
+		Categoria cat1 =  new Categoria(1,"Informatica");
+		Categoria cat2 =  new Categoria(2,"Escritorio");
+		List<Categoria> lista  = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
+		
+		
 	}
 
 }
